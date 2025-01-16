71b561e0b9cd251c48cb47e019fa62c1f181aa3c
Aarya Shinde <aaryashinde2020@gmail.com>
2025-01-16 11:17:36 +0530

    gym program 2

---
+++ gym.py
@@ -0,0 +1,8 @@
+print("Welcome to pokemon Gym");
+age = int(input("Enter your age"));
+
+
+if age >= 21 and age < 40 :
+    print('You can enroll in our gym');
+else: 
+  print('sorry, not eligible');

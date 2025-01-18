e207cccd4d0f804f456b431345054cc2f0334086
Aarya Shinde <aaryashinde2020@gmail.com>
2025-01-18 19:46:55 +0530

    login file

---
+++ controlstatement.py
@@ -0,0 +1,34 @@
+# Break the if 
+
+for i in range(1 , 11) :
+    if i ==6 :
+        break;
+    else :
+        print(i);
+print("Program Finished");
+
+
+for i in range(1 , 11) :
+    if i ==6 :
+        exit();
+    else :
+        print(i);
+print("Program Finished");
+
+
+
+for i in range(1 , 11) :
+    if i ==6 :
+        continue;
+    else :
+        print(i);
+print("Program Finished");
+
+
+
+for i in range(1 , 11) :
+    if i ==6 :
+        pass;
+    else :
+        print(i);
+print("Program Finished");
---
+++ login.py
@@ -0,0 +1,55 @@
+'''Creating a user registration and login page'''
+
+database = {"aarya" : "1234"}
+
+
+while True  : 
+
+    print('''
+Enter the Move:
+      1. Register 
+      2. Login
+    ''');
+
+    move = int(input('1 or 2- '));
+    
+    if move == 1 :
+        print('''
+        Enter the following Credentials for Registration:
+        ''');
+        username = input("Enter your Username - ");
+        password = input("\nEnter your PassWord - ");
+        confpassword = input("\nEnter your Pass Word again - ");
+
+        if password == confpassword :
+                print("Password Matches");
+                print("\n Registration Successful");
+                break;
+        else :
+                print("Password dosen't match");
+         
+      
+
+    elif move == 2 :
+        print('''
+        Enter your following Credentials:
+        ''');
+        username = input("\nEnter your Username - ");
+             
+        if username in database :
+            password = input("Enter your Password: ")
+            if database[username] == password :
+                  print("Login Sucessfull");
+            else :
+                  print ("Password dosen't match")
+        else :
+             print ("Login failed");
+        break;
+    
+    else : 
+        print("PLEASE TRY AGAIN!!");
+         
+
+
+
+        

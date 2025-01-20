b56d277c70f91e9ebab537e50262468873a74595
Aarya Shinde <aaryashinde2020@gmail.com>
2025-01-20 10:40:04 +0530

    Updating the login page

--- login.py
+++ login.py
@@ -19,11 +19,14 @@ Enter the Move:
         ''');
         username = input("Enter your Username - ");
         password = input("\nEnter your PassWord - ");
+
         confpassword = input("\nEnter your Pass Word again - ");
 
         if password == confpassword :
                 print("Password Matches");
                 print("\n Registration Successful");
+
+                database.update({username : password})
                 break;
         else :
                 print("Password dosen't match");
@@ -46,10 +49,8 @@ Enter the Move:
              print ("Login failed");
         break;
     
+    
     else : 
         print("PLEASE TRY AGAIN!!");
          
-
-
-
-        
+print(database);
